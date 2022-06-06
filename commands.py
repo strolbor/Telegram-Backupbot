8aa1e7864546336a0c4068eb3224b5b91c80f6a5
Urs-Benedict Braun <urs-benedict.braun@st.ovgu.de>
2022-06-06 16:50:46 +0200

    fix

--- commands.py
+++ commands.py
@@ -15,7 +15,7 @@ def test(update: Update, context: CallbackContext) -> None:
 
 def save(update: Update, context: CallbackContext) -> None:
     a = write.getLogLine(update.effective_user.id,"Save Command")
-    print(a,update.message.texts)
+    print(a,update.message.text)
     write.write_log(a)
     write.write_id(f'{update.effective_user.id}')
     write.write_txt(update.message.text)
